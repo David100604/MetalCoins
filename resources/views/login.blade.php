719966c46401fe7ff5f08c311bef05fca8d0b2cc
David100604 <davidvn1006@gmail.com>
2024-04-05 09:52:53 -0300

    Criação da tela de login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('content')
+
+<main id="login_container">
+    <form id="login_form">
+        <div id="login_form_header">
+            <h1>Login</h1>
+        </div>
+
+        <div id="login_inputs">
+
+            <div class="login_input-box">
+                <label for="username">
+                    Nome de Usuário
+                    <div class="login-input-field">
+                        <input type="text" id="username" name="username">
+                    </div>
+                </label>
+            </div>
+
+            <div class="login-input-box">
+                <label for="password">
+                    Senha
+                    <div class="login-input-field">
+                        <input type="password" id="password" name="password">
+                    </div>
+                </label>
+            </div>
+        </div>
+
+        <div id="login_buttons">
+
+            <div class="login-button">
+                <button type="submit" id="login_button">
+                    Entrar
+                </button>
+            </div>
+
+            <div class="login-button">
+                <button type="submit" id="forgot_password">
+                    Esqueci minha senha
+                </button>
+            </div>
+
+        </div>
+        
+    </form>
+</main>
+  
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/signin', function () {
+    return view('login');
+});
+
+Route::get('/cadastrar-produto', function () {
+    return view('form-incluir-produto');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
